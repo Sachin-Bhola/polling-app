6451c536e4602db6d7dbca0b536d799ab5755760
sachin <sbsachin699@gmail.com>
2019-09-24 18:23:57 +0530

    completed create poll task

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,24 @@ import { LoginComponent } from './login/login.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { RegistrationComponent } from './registration/registration.component';
 import { UserDashboardComponent } from './user-dashboard/user-dashboard.component';
+import { CreatePollComponent } from './create-poll/create-poll.component';
+import { ViewPollComponent } from './view-poll/view-poll.component';
+import { TakePollComponent } from './take-poll/take-poll.component';
 
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: "full" },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegistrationComponent },
-  { path: 'userdashboard', component: UserDashboardComponent },
+  {
+    path: 'userdashboard', component: UserDashboardComponent, children:
+      [
+        { path: '', redirectTo: 'createpoll', pathMatch: "full" },
+        { path: 'createpoll', component: CreatePollComponent },
+        { path: 'viewpoll', component: ViewPollComponent },
+        { path: 'takepoll', component: TakePollComponent }
+      ]
+  },
   { path: '**', component: PageNotFoundComponent }
 ];
 
---
+++ src/app/take-poll/take-poll.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-take-poll',
+  templateUrl: './take-poll.component.html',
+  styleUrls: ['./take-poll.component.css']
+})
+export class TakePollComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
